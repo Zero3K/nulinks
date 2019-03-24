94281dc7c08a12262f45e20e4ecb6836c4b686d0
Sahil Agarwal <sahil.agarwal94@gmail.com>
2019-03-24 23:38:07 +0530

    Add admin site

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from files.models import TorrentFile
 
 # Register your models here.
+
+NO_SEARCH_FIELDS = {"uploadTime", "location", "id"}
+
+UNEDITABLE_FIELDS = {"uploadTime", "id"}
+
+NO_FILTER_FIELDS = {"uploadTime", "location", "id"}
+
+
+class MyModelAdmin(admin.ModelAdmin):
+
+    ALL_FIELDS = set(i.name for i in TorrentFile._meta.fields)
+    foreign_keys = [
+        i.name
+        for i in TorrentFile._meta.fields
+        if i.get_internal_type() == "ForeignKey"
+    ]
+    list_display = [i.name for i in TorrentFile._meta.fields]
+    list_editable = tuple(ALL_FIELDS - UNEDITABLE_FIELDS)
+
+    list_display_links = None
+    list_filter = tuple(ALL_FIELDS - NO_FILTER_FIELDS)
+    search_fields = tuple(ALL_FIELDS - NO_SEARCH_FIELDS)
+    autocomplete_fields = foreign_keys
+    list_per_page = 10
+
+admin.site.register(TorrentFile, MyModelAdmin)
