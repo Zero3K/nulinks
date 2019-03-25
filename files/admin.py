ce52cbbef23b42c469ff2b1349dfe8b67a60a923
Sahil Agarwal <sahil.agarwal94@gmail.com>
2019-03-25 21:26:52 +0530

    Add MtCategory

--- files/admin.py
+++ files/admin.py
@@ -1,30 +1,32 @@
 from django.contrib import admin
-from files.models import TorrentFile
+from files.models import TorrentFile, MtCategory
 
 # Register your models here.
 
-NO_SEARCH_FIELDS = {"uploadTime", "location", "id"}
+NO_SEARCH_FIELDS = {'created_at', 'modified_at', "uploadTime", "location", "id"}
 
-UNEDITABLE_FIELDS = {"uploadTime", "id"}
+UNEDITABLE_FIELDS = {'created_at', 'modified_at', "uploadTime", "id"}
 
 NO_FILTER_FIELDS = {"uploadTime", "location", "id"}
 
+MODELS = (TorrentFile, MtCategory)
 
-class MyModelAdmin(admin.ModelAdmin):
+for my_model in MODELS:
+    class MyModelAdmin(admin.ModelAdmin):
 
-    ALL_FIELDS = set(i.name for i in TorrentFile._meta.fields)
-    foreign_keys = [
-        i.name
-        for i in TorrentFile._meta.fields
-        if i.get_internal_type() == "ForeignKey"
-    ]
-    list_display = [i.name for i in TorrentFile._meta.fields]
-    list_editable = tuple(ALL_FIELDS - UNEDITABLE_FIELDS)
+        ALL_FIELDS = set(i.name for i in my_model._meta.fields)
+        foreign_keys = [
+            i.name
+            for i in my_model._meta.fields
+            if i.get_internal_type() == "ForeignKey"
+        ]
+        list_display = [i.name for i in my_model._meta.fields]
+        list_editable = tuple(ALL_FIELDS - UNEDITABLE_FIELDS)
 
-    list_display_links = None
-    list_filter = tuple(ALL_FIELDS - NO_FILTER_FIELDS)
-    search_fields = tuple(ALL_FIELDS - NO_SEARCH_FIELDS)
-    autocomplete_fields = foreign_keys
-    list_per_page = 10
+        list_display_links = None
+        list_filter = tuple(ALL_FIELDS - NO_FILTER_FIELDS)
+        search_fields = tuple(ALL_FIELDS - NO_SEARCH_FIELDS)
+        autocomplete_fields = foreign_keys
+        list_per_page = 10
 
-admin.site.register(TorrentFile, MyModelAdmin)
+    admin.site.register(my_model, MyModelAdmin)
