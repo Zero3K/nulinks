864f573419195f68eb3ea0ebfc7e80339a1ae216
Sahil Agarwal <sahil.agarwal94@gmail.com>
2019-03-25 21:27:02 +0530

    Add MtCategory

--- files/models.py
+++ files/models.py
@@ -1,7 +1,35 @@
 from django.db import models
 
-class TorrentFile(models.Model):
+
+class TimestampFields(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = True
+        abstract = True
+
+
+class TorrentFile(TimestampFields):
     name = models.CharField(max_length=255)
     uploader = models.CharField(max_length=25)
     location = models.CharField(max_length=255)
-    uploadTime = models.DateTimeField(auto_now = False, auto_now_add = True)
+    uploadTime = models.DateTimeField(auto_now=False, auto_now_add=True)
+    category = models.ForeignKey('MtCategory', models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = True
+
+    def __str__(self):
+        return self.name.title()
+
+
+class MtCategory(TimestampFields):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        managed = True
+        db_table = 'mt_category'
+
+    def __str__(self):
+        return self.name.title()
